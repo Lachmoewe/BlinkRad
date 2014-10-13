4b1cf0bcd29c7cf328bfa3bdfbc283fc69b41d3a
Timo Fuckner <omg-lachmoewe@gmx.net>
2014-10-14 00:00:54 +0200

    working on communication

--- software/ledboard/main.c
+++ software/ledboard/main.c
@@ -1,47 +1,70 @@
 
-#define F_CPU 80000000UL
+#define F_CPU 8000000UL
 #include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
+#include <stdint.h>
 
-uint8_t r=200;
-uint8_t g=64;
-uint8_t b=64;
-uint8_t count = 1;
+uint8_t r,g,b;
+uint8_t pwm_count;
+uint8_t oldstate;
+
+uint8_t databit;
+uint16_t data;
 
 int PWM(void) {
-    if (count == 0) {
-        PORTB = 0b00011100;
+    if (pwm_count == 0) {
+        PORTB |= (1<<PB3)|(1<<PB4)|(1<<PB5); //0b00011100;
     }
-    if (r == count) {
-        PORTB &= 0b11101111; //r
+    if (pwm_count == r) {
+        PORTB &= ~(1<<PB3); //0b11101111; //r
     }
-    if (g == count) {
-        PORTB &= 0b11110111; //g
+    if (pwm_count == g) {
+        PORTB &= ~(1<<PB4); //0b11110111; //g
     }
-    if (b == count) {
-        PORTB &= 0b11111011; //b
+    if (pwm_count == b) {
+        PORTB &= ~(1<<PB5); //0b11111011; //b
     }
-    count++;
+    pwm_count++;
 }
 
 int main(void) {
 
-    DDRB = 0b00011100;
+    DDRB = 0b00111000;
     // Set up Port B data to be all low
     PORTB = 0;  
+    
+    r =  11;
+    g =  64;
+    b = 255;
 
-    sei();
+    pwm_count = 0;
+    oldstate = 0;
+
     while (1) {
         // Toggle Port B pin 4 output state
+        if (PORTB && (1<<PB2) && !oldstate) { // check if sck rised
+            oldstate = 1;
 
+            // read datain pin
+            databit = (PORTB && (1<<PB0));
+            data = (data<<1);
+            data |= databit;
+        
+        } else if (!(PORTB && (1<<PB2)) && oldstate){ // check if sck fell
+            oldstate = 0;
+
+            // write dataout pin
+            if (databit) {
+                PORTB |= (1<<PB1);
+            } else {
+                PORTB &= ~(1<<PB1);
+            }
+        }
+        PWM();
         _delay_us(1);
     }
 
     return 0;
 }
 
-ISR (_VECTOR(4)) {
-    PWM();
-}
