807931461325b398460800ef92d73835270cdf43
Timo Fuckner <omg-lachmoewe@gmx.net>
2014-10-15 16:45:46 +0200

    added some pictures

--- software/teensy/src/main.cpp
+++ software/teensy/src/main.cpp
@@ -1,28 +1,40 @@
 #include "WProgram.h"
 
+uint16_t display[37] = {
+    //xxsurrrgggbbb
+    0b0011111111111,
+    0x0000,0x0000,0x0000,0x0000,
+    0x0000,0x0000,0x0000,0x0000,
+    0x0000,0x0000,0x0000,0x0000,
+    0x0000,0x0000,0x0000,0x0000,
+    0x0000,0x0000,0x0000,0x0000,
+    0x0000,0x0000,0x0000,0x0000,
+    0x0000,0x0000,0x0000,0x0000,
+    0x0000,0x0000,0x0000,0x0000,
+    0x0000,0x0000,0x0000,0x0000    
+};
+
+void setLed(uint8_t nr, uint8_t r, uint8_t g, uint8_t b) {
+    if (nr<36) {
+        display[nr+1] = (r<<8) + (g<<2) + b;
+    }
+}
+
+uint8_t nextBit(void) {
+    static uint16_t bitpos;
+    
+}
+
 extern "C" int main(void)
 {
-#if !defined(ARDUINO)
-
-	// To use Teensy 3.0 without Arduino, simply put your code here.
-	// For example:
 
 	pinMode(13, OUTPUT);
+    setLed(0,15,8,0);
 	while (1) {
 		digitalWriteFast(13, HIGH);
 		delay(500);
 		digitalWriteFast(13, LOW);
 		delay(500);
 	}
-
-
-#else
-	// Arduino's main() function just calls setup() and loop()....
-	setup();
-	while (1) {
-		loop();
-		yield();
-	}
-#endif
 }
 
